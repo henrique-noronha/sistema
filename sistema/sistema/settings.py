8c460fb8b621d50d9b171fa18a0cdc625af80bbb
henrique-noronha <henriquenoronha020@gmail.com>
2026-04-15 19:28:59 -0300

    .

--- sistema/sistema/settings.py
+++ sistema/sistema/settings.py
@@ -91,13 +91,9 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sistemacars',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-}
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
